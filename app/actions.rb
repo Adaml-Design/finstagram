70042d5042787c83c995ae328fda74c104db71cd
Adaml-Design <adamvawesome@gmail.com>
2021-03-30 03:48:31 +0000

    clean up 3

--- app/actions.rb
+++ app/actions.rb
@@ -1,7 +1,7 @@
 helpers do
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
 end
 
 get '/' do
@@ -11,12 +11,12 @@ get '/' do
 end
 
 get '/signup' do     # if a user navigates to the path "/signup",
-  @user = User.new   # setup empty @user object
-  erb(:signup)       # render "app/views/signup.erb"
+    @user = User.new   # setup empty @user object
+    erb(:signup)       # render "app/views/signup.erb"
 end
 
 get '/login' do    # when a GET request comes into /login
-  erb(:login)      # render app/views/login.erb
+    erb(:login)      # render app/views/login.erb
 end
 
 get '/logout' do
@@ -45,50 +45,25 @@ post '/signup' do
     end
 end
 
-# post '/login' do    # when a GET request comes into /login
-#     # params.to_s  # just display the params for now to make sure it's working
-#     username = params[:username]
-#     password = params[:password]
-
-#     # 1. find user by username
-#     user = User.find_by(username: username)
-
-#     # 2. if that user exists
-#     if user
-
-#         # check if that user's password matches the password input
-#         # 3. if the passwords match
-#         if user.password == password
-
-#             # login (more to come here)
-#             "Success!"
-#         else
-#             "Password doesn't match"
-#         end
-#     else
-#         "No user"
-#     end
-# end
-
 post '/login' do
-  username = params[:username]
-  password = params[:password]
+    username = params[:username]
+    password = params[:password]
 
-  @user = User.find_by(username: username)
+    @user = User.find_by(username: username)
 
-  if @user && @user.password == password
-    session[:user_id] = @user.id
-    # "Success! User with id #{session[:user_id]} is logged in!"
-    redirect to('/')
-  else
-    @error_message = "Login failed."
-    erb(:login)
-  end
+    if @user && @user.password == password
+        session[:user_id] = @user.id
+        # "Success! User with id #{session[:user_id]} is logged in!"
+        redirect to('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
 end
 
 get '/finstagram_posts/new' do
-  @finstagram_post = FinstagramPost.new
-  erb(:"finstagram_posts/new")
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
 end
 
 post '/finstagram_posts' do
